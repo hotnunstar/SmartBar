caa5b480841e389776eaba616b69ae3b38fd5bcc
Nuno Miguel Carvalho Araújo <nu-nos@hotmail.com>
2022-12-17 12:55:14 +0000

    backend authentication done

---
+++ Source/Backend/SmartBar/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartBar.Helpers;
+using SmartBar.Models;
+using SmartBar.Services;
+
+namespace SmartBar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService) => _userService = userService;
+
+        [HttpGet]
+        public async Task<UserModel> GetUser(string? email, string? password)
+        {
+            if(email != null && password != null)
+            {
+                password = Functions.EncodePasswordToBase64(password);
+                var user = await _userService.GetAsync(email);
+                if (user == null || user.Password != password) return null;
+                else return user;
+            }
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostUser(UserModel user)
+        {
+            user.Id = ""; // Para atribuir ID default
+            user.Password = Functions.EncodePasswordToBase64(user.Password);
+
+            ResponseModel response = new()
+            {
+                StatusCode = 400,
+                Message = "Parâmetros inválidos"
+            };
+            // FALTA FAZER VERIFICAÇÕES DOS DADOS DE ENTRADA
+            if(Functions.CheckEmail(user.Email))
+            {
+                await _userService.CreateAsync(user);
+                return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+            }
+            return BadRequest(response);
+        }
+    }
+}
---
+++ Source/Backend/SmartBar/Services/ColaboratorService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using SmartBar.Models;
+
+namespace SmartBar.Services
+{
+    public class ColaboratorService
+    {
+        private readonly IMongoCollection<ColaboratorModel> _colaboratorCollection;
+
+        public ColaboratorService(IOptions<SmartBarDatabaseSettingsModel> smartBarDatabaseSettingsModel)
+        {
+            var mongoClient = new MongoClient(smartBarDatabaseSettingsModel.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(smartBarDatabaseSettingsModel.Value.DatabaseName);
+            _colaboratorCollection = mongoDatabase.GetCollection<ColaboratorModel>(smartBarDatabaseSettingsModel.Value.ColaboratorCollectionName);
+        }
+
+        public async Task<List<ColaboratorModel>> GetAsync() =>
+            await _colaboratorCollection.Find(_ => true).ToListAsync();
+
+        public async Task<ColaboratorModel?> GetAsync(string email) =>
+        await _colaboratorCollection.Find(x => x.Email == email).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(ColaboratorModel newColaborator) =>
+            await _colaboratorCollection.InsertOneAsync(newColaborator);
+
+        public async Task UpdateAsync(string id, ColaboratorModel updatedColaborator) =>
+            await _colaboratorCollection.ReplaceOneAsync(x => x.Id == id, updatedColaborator);
+
+        public async Task RemoveAsync(string id) =>
+            await _colaboratorCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
