5c1567bacc8ea8da883804065530531c24ef770d
JoelFigueiras <joelfigueiras@live.com.pt>
2023-01-06 16:47:32 +0000

    update request controller

--- Source/Backend/SmartBar/Controllers/RequestController.cs
+++ Source/Backend/SmartBar/Controllers/RequestController.cs
@@ -12,21 +12,22 @@ namespace SmartBar.Controllers
     {
         private readonly RequestService _resquestService;
         private readonly ProductService _productService;
-
+        private readonly UserService _userService;
 
         /// <summary>
         /// Construtor do controlador de pedidos
         /// </summary>
         /// <param name="resquestService"></param>
         /// <param name="productService"></param>
-        public RequestController(RequestService resquestService, ProductService productService)
+        public RequestController(RequestService resquestService, ProductService productService, UserService userService)
         {
             _resquestService = resquestService;
             _productService = productService;
+            _userService = userService;
         }
 
 
-        
+
 
         [HttpGet]
         public async Task<List<RequestModel>> GetAll()
@@ -47,8 +48,20 @@ namespace SmartBar.Controllers
             List<ProductModel> productsList = await _productService.GetAsync(); //lista de produtos
             List<ProductRequest> productRequest = new List<ProductRequest>(); 
             productRequest = request.ProductAndQuantity; // lista de produtos ao pedido do cliente 
+            UserModel user = await _userService.GetAsyncById(request.IdCliente);
+            double auxSaldo = 0;
+            double auxSaldoInicial = user.Balance;
             int aux = 0;
-
+            auxSaldo = user.Balance - request.Value;
+            if(auxSaldo < 0)
+            {
+                return BadRequest($"Não existe saldo suficiente para efetuar a compra");
+            }
+            else
+            {
+                user.Balance = auxSaldo;
+                await _userService.UpdateAsync(request.IdCliente, user);
+            }
             foreach (ProductModel product in productsList)
             {
                 foreach (ProductRequest productRequestItem in productRequest)
@@ -58,12 +71,15 @@ namespace SmartBar.Controllers
                         aux = product.Stock - productRequestItem.Quantity;
                         if(aux < 0)
                         {
+                            user.Balance = auxSaldoInicial;
+                            await _userService.UpdateAsync(request.IdCliente, user);
                             return BadRequest($"Não existe stock suficiente do produto {product.Name}");
                         }
+                        product.Stock = aux;
+                       await _productService.UpdateAsync(productRequestItem.IdProduct,product);
                     }
                 }
             }
-            
             request.IdRequest = ""; //Atribuir ID default
             request.State = 1; //Estado Inicial
             request.DateRequest = DateTime.Now;
