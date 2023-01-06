1a36748183bfb68222d1cf25e65358ccd694acbf
Nuno Miguel Carvalho Araújo <nu-nos@hotmail.com>
2023-01-06 14:52:36 +0000

    backend encode password update

--- Source/Backend/SmartBar/Helpers/Functions.cs
+++ Source/Backend/SmartBar/Helpers/Functions.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Data.SqlTypes;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SmartBar.Helpers
 {
@@ -8,6 +10,26 @@ namespace SmartBar.Helpers
     /// </summary>
     public class Functions
     {
+        /// <summary>
+        /// Encriptar passwords
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns>Password encriptada</returns>
+        /// <exception cref="Exception"></exception>
+        public static string EncodePassword(string password)
+        {
+            var md5 = MD5.Create();
+            byte[] bytes = Encoding.ASCII.GetBytes(password);
+            byte[] hash = md5.ComputeHash(bytes);
+
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
         /// <summary>
         /// Verificar se determinado email está num formato correto
         /// </summary>
@@ -21,44 +43,6 @@ namespace SmartBar.Helpers
                 return true;
             }
             catch { return false; }
-        }
-
-        /// <summary>
-        /// Encriptar passwords
-        /// </summary>
-        /// <param name="password"></param>
-        /// <returns>Password encriptada</returns>
-        /// <exception cref="Exception"></exception>
-        public static string EncodePasswordToBase64(string password)
-        {
-            try
-            {
-                byte[] encData_byte = new byte[password.Length];
-                encData_byte = Encoding.UTF8.GetBytes(password);
-                string encodedData = Convert.ToBase64String(encData_byte);
-                return encodedData;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error in base64Encode" + ex.Message);
-            }
-        }
-
-        /// <summary>
-        /// Desencriptar password
-        /// </summary>
-        /// <param name="encodedData"></param>
-        /// <returns>Password desencriptada</returns>
-        public string DecodeFrom64(string encodedData)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            Decoder utf8Decode = encoder.GetDecoder();
-            byte[] todecode_byte = Convert.FromBase64String(encodedData);
-            int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
-            char[] decoded_char = new char[charCount];
-            utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
-            string result = new (decoded_char);
-            return result;
         }
     }
 }
