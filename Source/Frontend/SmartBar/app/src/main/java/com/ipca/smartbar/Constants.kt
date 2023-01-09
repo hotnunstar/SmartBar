5534682386d08a024b432602cbf89a5f21b1b063
JoelFigueiras <joelfigueiras@live.com.pt>
2023-01-09 14:20:35 +0000

    constants

--- Source/Frontend/SmartBar/app/src/main/java/com/ipca/smartbar/Constants.kt
+++ Source/Frontend/SmartBar/app/src/main/java/com/ipca/smartbar/Constants.kt
@@ -1,5 +1,5 @@
 package com.ipca.smartbar
 
 object Constants {
-    const val baseurl = "https://7e83-2001-8a0-f42e-801-f476-721b-cf80-b784.eu.ngrok.io"
+    const val baseurl = "https://4cf7-2001-8a0-fe28-f600-e98e-4dcd-3c91-59bf.eu.ngrok.io"
 }
