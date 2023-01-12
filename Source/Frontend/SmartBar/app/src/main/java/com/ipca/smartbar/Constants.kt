4183f4f39e7bd2de738b23f84cb0d5ec0e164622
Nuno Miguel Carvalho Araújo <nu-nos@hotmail.com>
2023-01-12 15:41:03 +0000

    baseURL with published API

--- Source/Frontend/SmartBar/app/src/main/java/com/ipca/smartbar/Constants.kt
+++ Source/Frontend/SmartBar/app/src/main/java/com/ipca/smartbar/Constants.kt
@@ -2,5 +2,5 @@ package com.ipca.smartbar
 
 object Constants {
 
-    const val baseurl = "https://b8bb-2001-8a0-f42e-801-d07d-cc84-4e7d-e63.eu.ngrok.io"
+    const val baseurl = "https://ipcasmartbar.azurewebsites.net"
 }
